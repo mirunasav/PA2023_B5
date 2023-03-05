419515e08340dd99ee38107531fbe283cfd11673
mirunasav <miruna.savin02@gmail.com>
2023-03-05 20:05:42 +0200

    compulsory

---
+++ Laborator3/src/compulsory/Node.java
@@ -0,0 +1,9 @@
+package compulsory;
+
+public interface Node {
+    //if I define a variable here it will be public, static and final
+    //I can ommit writing the public modifiers since all abstract, default & static methods
+    //of an interface are implicitly public
+    String getName ();
+
+}
