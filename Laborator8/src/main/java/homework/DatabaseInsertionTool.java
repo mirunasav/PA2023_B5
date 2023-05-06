cfa901f25fece73bc3a39f2b7d0df7422273ec93
Savin Miruna <miruna.savin02@gmail.com>
2023-05-06 13:49:27 +0300

    insert album_genre associations using database insertion tool

--- Laborator8/src/main/java/homework/DatabaseInsertionTool.java
+++ Laborator8/src/main/java/homework/DatabaseInsertionTool.java
@@ -1,6 +1,7 @@
 package homework;
 
 import compulsory.Database;
+import org.postgresql.util.PSQLException;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -80,7 +81,6 @@ public class DatabaseInsertionTool {
     public static void addAlbumGenreAssociations(){
         try {
             Connection connection = DBCPDataSource.getConnection();
-
             String insertAssociation = "INSERT INTO album_genres (album_id, genre_id) VALUES (?,?)";
 
             PreparedStatement associationStatement = connection.prepareStatement(insertAssociation);
@@ -102,39 +102,53 @@ public class DatabaseInsertionTool {
                 Statement searchGenre = connection.createStatement();
                 ResultSet rs = searchGenre.executeQuery("select * from genres where name ='" + genreName + "'");
                 int genreID =  rs.next() ? rs.getInt(1) : 0;
+                rs.close();
                 if (genreID == 0){
                     continue;
                 }
+                System.out.println("am gasit genre id " + genreID);
                 //caut albumul
                 Statement searchAlbum = connection.createStatement();
                 ResultSet rsAlbum = searchAlbum.executeQuery("select * from albums where title='" + albumTitle + "'");
                 int albumId = rsAlbum.next() ? rsAlbum.getInt(1) : 0;
+                rsAlbum.close();
 
                 if (albumId == 0){
                     continue;
                 }
+                System.out.println("am gasit album id "+ albumId);
                 //altfel: am gasit atat album id cat si artist i
 
                 associationStatement.setInt(1,albumId);
                 associationStatement.setInt(2,genreID);
 
-                associationStatement.addBatch();
+
+                count++;
+                try{
+                    associationStatement.executeUpdate();
+                }
+                catch (PSQLException e){
+                    continue;
+                }
+                System.out.println("inca 1");
+                System.out.println(count);
+
+              /*  associationStatement.addBatch();
                 count++;
                 numberOfAlbumsAdded++;
 
-                System.out.println("inca 1");
                 if(count% batchSize == 0) {
                     associationStatement.executeBatch();
                     count = 0;
-                }
+                }*/
             }
 
             lineReader.close();
-            associationStatement.executeBatch();
             DBCPDataSource.getConnection().commit();
             DBCPDataSource.getConnection().close();
         } catch (SQLException e) {
             System.err.println(e);
+            e.printStackTrace();
             try {
                 Database.getConnection().rollback();
             } catch (SQLException error) {
@@ -148,68 +162,6 @@ public class DatabaseInsertionTool {
     }
     public static void main(String[] args) {
         //DatabaseInsertionTool.addAlbums();
-        DatabaseInsertionTool.addAlbumGenreAssociations();
-        /*try {
-            Connection connection = DBCPDataSource.getConnection();
-
-            String insertArtistSQL = "INSERT INTO artists (name) VALUES (?)";
-            String insertAlbumSQL = "insert into albums(release_year,title,artist) values(?,?,?)";
-            String insertGenreSQL = "insert into genres(name) values(?)";
-            String insertGenreAlbumAssociation = "insert into album_genres(album_id, genre_id) values(?,?)";
-
-            PreparedStatement artistStatemet = connection.prepareStatement(insertArtistSQL);
-            PreparedStatement albumStatement = connection.prepareStatement(insertAlbumSQL);
-            PreparedStatement genreStatement = connection.prepareStatement(insertGenreSQL);
-            PreparedStatement genreAlbumStatement = connection.prepareStatement(insertGenreAlbumAssociation);
-
-            BufferedReader lineReader = new BufferedReader(new FileReader(csvFilePath));
-            String lineText = null;
-
-            int count = 0;
-
-            lineReader.readLine(); // skip header line
-
-            while ((lineText = lineReader.readLine()) != null) {
-                String[] data = lineText.split(",");
-                String releaseYear = data[1];//asta tb sa fac cast la int
-                int releaseYearInt = Integer.parseInt(releaseYear);
-                String albumName = data[2];
-                String artistName = data[3];
-                String genreName = data[4];
-
-                artistStatemet.setString(1, artistName);
-                artistStatemet.executeUpdate();
-
-                Artist artist =
-                        albumStatement.setInt(1, releaseYearInt);
-                albumStatement.setString(2, albumName);
-                albumStatement.setInt(3, albumName);//fac pe rand mai bine?
-                statement.setString(2, studentName);
-
-
-                statement.setString(5, comment);
-
-                statement.addBatch();
-
-                if (count % batchSize == 0) {
-                    statement.executeBatch();
-                }
-            }
-
-            lineReader.close();
-            Database.getConnection().commit();
-            Database.getConnection().close();
-        } catch (SQLException e) {
-            System.err.println(e);
-            try {
-                Database.getConnection().rollback();
-            } catch (SQLException error) {
-                System.err.println("yet another error");
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+       DatabaseInsertionTool.addAlbumGenreAssociations();
     }
 }
