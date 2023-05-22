a0414bbd0e2a1de556781194f93503de7aaf6db0
Savin Miruna <miruna.savin02@gmail.com>
2023-05-22 19:17:33 +0300

    homework requests

--- Laborator11/src/main/java/com/example/laborator11/UsersDB.java
+++ Laborator11/src/main/java/com/example/laborator11/UsersDB.java
@@ -4,18 +4,43 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Component
 public class UsersDB {
     public static List<User> userList = new ArrayList<>();
 
-    static{
+    static {
         userList.add(new User("user1", 1));
         userList.add(new User("user2", 2));
         userList.add(new User("user3", 3));
     }
 
-    public static List<User> getAll (){
+    public static List<User> getAll() {
         return userList;
+    }
+
+    public static User save(User user) {
+        userList.add(user);
+        return user;
+    }
+
+    public static List<User> delete(String username) {
+        userList.removeIf(object -> Objects.equals(object.getUsername(), username));
+        return userList;
+    }
+
+    public static void changeUsername(String oldUsername, String newUsername) {
+        boolean userFound = userList.stream()
+                .filter(user -> user.getUsername().equals(oldUsername))
+                .peek(user -> user.setUsername(newUsername))
+                .findFirst()
+                .isPresent();
+        if (!userFound) {
+            throw new IllegalArgumentException("User with old username not found");
+        }
+
     }
 }
--- Laborator11/src/main/java/com/example/laborator11/controllers/UserController.java
+++ Laborator11/src/main/java/com/example/laborator11/controllers/UserController.java
@@ -2,9 +2,8 @@ package com.example.laborator11.controllers;
 
 import com.example.laborator11.User;
 import com.example.laborator11.UsersDB;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,5 +15,30 @@ public class UserController {
     @GetMapping
     public List<User> getListOfUsers(){
         return UsersDB.userList;
+    }
+    
+    @PostMapping(path = "/users")
+     public User createUser(@RequestBody User user)  {
+       return UsersDB.save(user);
+    }
+
+    @DeleteMapping(path = "/users/{username}")
+    @ResponseBody
+    public List<User> deleteUser (@PathVariable String username)
+    {
+        return UsersDB.delete(username);
+    }
+
+    @PutMapping(path = "/users/change/{oldUsername}")
+    public ResponseEntity<String> changeUsername(
+            @PathVariable String oldUsername,
+            @RequestParam String newUsername) {
+       try{
+           UsersDB.changeUsername(oldUsername, newUsername);
+       }
+       catch(Exception ex){
+           return (ResponseEntity<String>) ResponseEntity.badRequest();
+       }
+        return ResponseEntity.ok("username update successfully");
     }
 }
